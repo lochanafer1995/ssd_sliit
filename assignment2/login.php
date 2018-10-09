fab445becf87c903d13d2ad8d29bf9d1e5d8a997
lochana <mayanjith1995@gmail.com>
2018-10-09 20:15:31 +0530

    change to googel drive

--- assignment2/login.php
+++ assignment2/login.php
@@ -1,12 +1,27 @@
-<?php
-//the code for :  login.php:
-?>
+<link href="bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="jquery-1.11.1.min.js"></script>
+<script src="bootstrap.min.js"></script>
+<link href="form.css" rel="stylesheet" id="bootstrap-css">
+<link href="font-awesome.min.css" rel="stylesheet">
+
+
 <html>
  <body>
-  <form action="https://www.facebook.com/dialog/oauth?response_type=code&client_id=2082020328498756&redirect_uri=https%3A%2F%2Flocalhost%2Fssd_sliit%2Fassignment2%2Fprocess.php&scope=public_profile%20user_posts%20user_friends%20user_photos%20email" method="post">
-   <input type="submit" value="Login" onclick="window.location.href='http://www.hyperlinkcode.com/button-links.php'"/>
-  </form>
-
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 col-md-4 col-md-offset-4">
+            <div class="account-wall" style="margin-top: 50%;">
+			<img class="profile-img" src="logo.png"
+                    alt="">
+                <form class="form-signin" action="https://accounts.google.com/o/oauth2/v2/auth?client_id=502184323359-o0a2086o2igadevkekqac7m3gq5l6j71.apps.googleusercontent.com&response_type=code&scope=https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/plus.me&redirect_uri=http://localhost/ssd_sliit/assignment2/process.php&access_type=offline" method="post">
+					<button class="btn btn-lg btn-primary btn-block" type="submit" style="background-color: #f44b42;">
+						Sign in with Google Account
+					</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
  </body>
 </html>
 
--- assignment2/process.php
+++ assignment2/process.php
@@ -1,37 +1,145 @@
+<link href="bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="jquery-1.11.1.min.js"></script>
+<script src="bootstrap.min.js"></script>
+<link href="style.css" rel="stylesheet" id="bootstrap-css">
+<link href="font-awesome.min.css" rel="stylesheet">
+
 <?php
-//The code for : process.php
-//session_start();
+
 $token = $_GET["code"];
 if (isset($token))
-{ ?>
-	<script src="https://localhost/ssd_sliit/assignment2/node_modules/jquery/dist/jquery.min.js"></script>
+{ 
+?>
+	<script src="node_modules/jquery/dist/jquery.min.js"></script>
         <script>
         //When the page has loaded.
-        $( document ).ready(function(){
-            //Perform Ajax request.
-            $.ajax({
-                url: 'https://graph.facebook.com/oauth/access_token',
-                type: 'post',
-				header : 'Authorization: Basic MjA4MjAyMDMyODQ5ODc1NjpjNTZkNTVlNTE0MDEyMWMzY2Q5NGZlMDgxMWFiNmI0Mw==',
-				data: { 
-					'grant_type': 'authorization_code',
-					'redirect_uri': 'https%3A%2F%2Flocalhost%2Fssd_sliit%2Fassignment2%2Fprocess.php',
-					'client_id':'2082020328498756',
-					'code': '<?php echo $_GET["code"]; ?>'
-				},
-                success: function(data){
-                    console.log(data);
-                },
-                error: function (xhr, ajaxOptions, thrownError) {
-                    var errorMsg = 'Ajax request failed: ' + xhr.responseText;
-                    //$('#content').html(errorMsg);
-                  }
-            });
-        });
-		
+			$( document ).ready(function(){
+				//Perform Ajax request.
+				$.ajax({
+					url: 'https://www.googleapis.com/oauth2/v4/token',
+					type: 'post',
+					header : 'Content-Type: application/x-www-form-urlencoded',
+					data: { 
+					
+						'code': '<?php echo $_GET["code"]; ?>',
+						'scope': 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/plus.me',
+						'client_id': '502184323359-o0a2086o2igadevkekqac7m3gq5l6j71.apps.googleusercontent.com',
+						'client_secret': 'q2Tsd8kabX0UHrAh1f8eto5Z',
+						'grant_type':'authorization_code',
+						'redirect_uri':'http://localhost/ssd_sliit/assignment2/process.php'
+						
+					},
+					success: function(data){
+						console.log(data);
+						getval(data.access_token);
+						getfiles(data.access_token);
+					},
+					error: function (xhr, ajaxOptions, thrownError) {
+						var errorMsg = 'Ajax request failed: ' + xhr.responseText;
+						//$('#content').html(errorMsg);
+					  }
+				});
+			});
+			function getval(value){
+				var url = 'https://www.googleapis.com/plus/v1/people/me?access_token='; 
+				var res = url.concat(value);
+				$.ajax({
+					url: res,
+					type: 'GET',
+					header : 'Content-Type: application/x-www-form-urlencoded',
+					success: function(data){
+							console.log(data);
+							$('#name').text(data.displayName);
+							$("#image1").attr("src",data.image.url);
+							$("#name").attr("href",data.url);
+					},
+					error: function (xhr, ajaxOptions, thrownError) {
+							var errorMsg = 'Ajax request failed: ' + xhr.responseText;
+							//$('#content').html(errorMsg);
+						  }
+				});
+			}
+			
+			function getfiles(value){
+				var url = 'https://www.googleapis.com/drive/v2/files?access_token='; 
+				var res = url.concat(value);
+				$.ajax({
+					url: res,
+					type: 'GET',
+					header : 'Content-Type: application/x-www-form-urlencoded',
+					success: function(data){
+							console.log(data.items);
+							var list = "";
+							for (var i = 0; i < data.items.length; i++) {
+								console.log(data.items[i].title);
+								list +="<li><a onclick=downloadfile('"+data.items[i].downloadUrl+"','"+value+"')>"+data.items[i].title+"</a></li>";
+							}
+							$("#gamelist").append(list);
+					},
+					error: function (xhr, ajaxOptions, thrownError) {
+							var errorMsg = 'Ajax request failed: ' + xhr.responseText;
+							//$('#content').html(errorMsg);
+						  }
+				});
+			}
+			function downloadfile(urlval,access){
+				var head = 'Authorization : Bearer '; 
+				var res = head.concat(access);
+				console.log(res);
+				$.ajax({
+					url: urlval,
+					type: 'GET',
+					header : res,
+					success: function(data){
+							console.log(data);
+					},
+					error: function (xhr, ajaxOptions, thrownError) {
+							var errorMsg = 'Ajax request failed: ' + xhr.responseText;
+							//$('#content').html(errorMsg);
+						  }
+				});
+				
+			}
         </script>
-<?php
-}
+<?php } ?>
 
-?>
-<!--<input type="text" name="user" value="<?php //echo $_GET["code"]; ?>"/> -->
+<div class="container">
+	<div class="row">
+		<div>
+            <div class="card hovercard">
+                <div class="cardheader">
+					
+                </div>
+                <div class="avatar">
+                    <img alt="" id="image1"/>
+                </div>
+                <div class="info">
+                    <div class="title">
+                        <a target="_blank" id="name"></a>
+                    </div>
+                </div>
+                <div class="bottom">           
+                </div>
+            </div>
+        </div>
+
+	</div>
+	<div class="row" >
+		<div>
+
+            <div class="card hovercard">
+                <div class="info">
+                    <div class="title">
+						<h3>Google drive files</h3>
+                    </div>
+					
+					 <ul id="gamelist"></ul>
+                </div>
+                <div class="bottom">
+                   
+                </div>
+            </div>
+
+        </div>
+</div>
+
